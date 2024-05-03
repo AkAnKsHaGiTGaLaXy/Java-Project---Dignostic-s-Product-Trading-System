34a3c492b9fe09809b8c1f74abe6cdfc4479fb95
Akanksha Chandrashekhar Ghatage <168676848+AkAnKsHaGiTGaLaXy@users.noreply.github.com>
2024-05-03 15:14:01 +0530

    Update cart.java

--- src/java/cart.java
+++ src/java/cart.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,10 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
- * @author prath
- */
+
 public class cart extends HttpServlet {
     
     Connection cn = null;
